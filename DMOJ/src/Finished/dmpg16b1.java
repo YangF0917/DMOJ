0845a7733f2747656b5948a3f8ff95e072144aa0
Felix <felix.yang19@ycdsbk12.ca>
2019-02-05 10:01:32 -0500

    Complete dmpg16b1

---
+++ DMOJ/src/Finished/dmpg16b1.java
@@ -0,0 +1,11 @@
+package Finished;
+import java.util.*;
+public class dmpg16b1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print(sc.nextInt() * 5 + sc.nextInt() * 10 + sc.nextInt() * 25 + sc.nextInt() * 100 + sc.nextInt() * 200);
+	}
+
+}
