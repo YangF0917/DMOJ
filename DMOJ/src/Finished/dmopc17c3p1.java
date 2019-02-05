3c5b16a04610b396e2232686ac1106e0ba386c8f
Felix <felix.yang19@ycdsbk12.ca>
2019-02-05 09:28:15 -0500

    Rename to match URL tag

--- DMOJ/src/Finished/dmopc17c3p1.java
+++ DMOJ/src/Finished/dmopc17c3p1.java
@@ -1,6 +1,6 @@
 package Finished;
 import java.util.*;
-public class AnEarlyChristmasPresent {
+public class dmopc17c3p1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
