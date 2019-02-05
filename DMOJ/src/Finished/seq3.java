b9ae0f09321b0c03cc63c35a1079bf0059964dc1
Felix <felix.yang19@ycdsbk12.ca>
2019-02-05 09:27:20 -0500

    Complete seq3

---
+++ DMOJ/src/Finished/seq3.java
@@ -0,0 +1,16 @@
+package Finished;
+import java.util.*;
+public class seq3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int p = sc.nextInt();
+		for (int i = 1; i < n; i++) {
+			System.out.print("0 ");
+		}
+		System.out.println(p);
+	}
+
+}
