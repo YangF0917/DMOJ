ca368610f77020d45fe9ef4d2adad66b9ee5380d
Felix <felix.yang19@ycdsbk12.ca>
2019-02-05 09:48:35 -0500

    Complete ccc09j1

---
+++ DMOJ/src/Finished/ccc09j1.java
@@ -0,0 +1,12 @@
+package Finished;
+import java.util.*;
+public class ccc09j1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("The 1-3-sum is " + (91 + sc.nextInt() + 3*sc.nextInt() + sc.nextInt()));
+		
+	}
+
+}
