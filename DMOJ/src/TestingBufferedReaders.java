d1b832ada181046c68dc9d1fe85a1c2fc6f6483e
Felix <felix.yang19@ycdsbk12.ca>
2019-02-19 20:38:56 -0500

    update to include stringtokenizer

--- DMOJ/src/TestingBufferedReaders.java
+++ DMOJ/src/TestingBufferedReaders.java
@@ -1,9 +1,15 @@
 import java.io.*;
+import java.util.*;
 public class TestingBufferedReaders {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		while (st.hasMoreTokens()) {
+			int a = Integer.parseInt(st.nextToken());
+			System.out.println(a);
+		}
 		String c = br.readLine();
 		System.out.println(c);
 		
