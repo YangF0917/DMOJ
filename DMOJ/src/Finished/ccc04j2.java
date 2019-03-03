d1f677cf1d967e739df9e9e3f3f21e193b313ec9
Felix <felix.yang19@ycdsbk12.ca>
2019-03-02 19:14:03 -0500

    complete ccc04j2

---
+++ DMOJ/src/Finished/ccc04j2.java
@@ -0,0 +1,13 @@
+package Finished;
+import java.util.*;
+public class ccc04j2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt(); int b = sc.nextInt();
+		while (a <= b) {System.out.println("All positions change in year " + a); a+=60;}
+		
+	}
+
+}
