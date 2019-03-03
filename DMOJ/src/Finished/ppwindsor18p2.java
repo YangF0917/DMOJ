41dce3f1a11fd9241acf66b0289bad61dc709fe5
Felix <felix.yang19@ycdsbk12.ca>
2019-03-02 19:13:32 -0500

    complete ppwindsor18p2

--- DMOJ/src/Finished/ppwindsor18p2.java
+++ DMOJ/src/Finished/ppwindsor18p2.java
@@ -1,23 +1,22 @@
 package Finished;
 import java.util.*;
+import java.io.*;
+import java.time.*;
 public class ppwindsor18p2 {
-
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException{
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		
 		int w = sc.nextInt(); int h = sc.nextInt();
-		
-		for (int i = 0; i < h; i++) {
-			for (int j = 0; j < w; j++) {
-				if ((i + j) % 2 == 0) {
-					System.out.print("0");
-				}
-				else {
-					System.out.print("1");
-				}
+		String [] line = {"",""};
+		for (int i = 0; i < w; i++) {
+			if (i%2 == 0) {
+				line[0] += "0"; line[1] += "1";
+			} else {
+				line[0] += "1"; line[1] += "0";
 			}
-			System.out.println();
+		}
+		for (int j = 0; j < h; j++) {
+			System.out.println(line[j%2]);
 		}
 	}
 
