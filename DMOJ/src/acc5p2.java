fd9f88fed4d3d393ff98984fcd3bd5bc4f318fc5
Felix <felix.yang19@ycdsbk12.ca>
2019-07-27 06:08:24 -0400

    Create acc5p2

--- DMOJ/src/acc5p2.java
+++ DMOJ/src/acc5p2.java
@@ -1,9 +1,10 @@
-
+import java.util.*;
 public class acc5p2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
 	}
 
 }
