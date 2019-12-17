c9d2f31c30124615ceb1c60a6fedc3ad205304ac
felix <felix@Ylixz-Laptop>
2019-12-16 23:16:00 -0500

    Complete acc5p1

---
+++ DMOJ/src/Finished/acc5p1.java
@@ -0,0 +1,12 @@
+package Finished;
+import java.util.*;
+public class acc5p1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int inp = sc.nextInt();
+		System.out.println(2*(1 + inp));
+	}
+
+}
